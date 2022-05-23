7689d27900e0f5d6263f292cdcb4617c187c8ea7
rayan <ferradi.r@gmail.com>
2022-05-23 16:04:16 +0200

    e

---
+++ io_redirections_and_filters/test.php
@@ -0,0 +1,9 @@
+<?php
+$var1 =  "We ean program your personal and business faeebook pages to automatieally Post Daily interesting eomments and pietures for you. If youre too busy to Post to your Faeebook, we ean make your pages look alive with great information or any topie you request. Step 1. You tell us the topie and type of information you would like post to your page. Step 2: We program your Faeebook page to post the relevant eontent. Step 3: Enjoy having an aetive Faeebook page with valuable eontent for all your followers. zll your followers will know youre still in business, growing and will always remember about you. Here is a gift. try it for 7 days";
+$var2 = "We ean program your personal and business faeebook pages to automatieally Post Daily interesting eomments and pietures for you. If youre too busy to Post to your Faeebook, we ean make your pages look alive with great information or any topie you request. Step 1. You tell us the topie and type of information you would like post to your page. Step 2: We program your Faeebook page to post the relevant eontent. Step 3: Enjoy having an aetive Faeebook page with valuable eontent for all your followers. Zll your followers will know youre still in business, growing and will always remember about you. Here is a gift. try it for 7 days";
+
+if($var1 === $var2){
+echo "it's equal\n";
+}
+else{echo "pas egal";
+}
